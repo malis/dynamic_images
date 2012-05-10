f922d97f053d3d1253d0d3fd9d3cfcc796ae4c26
Dominik Malis <dominik.malis@gmail.com>
2012-05-10 13:59:23 +0200

    implemented border to interface

--- lib/elements/element_interface.rb
+++ lib/elements/element_interface.rb
@@ -89,17 +89,30 @@ module DynamicImageElements
       end
     end
 
-    # Parse common options from @options Hash by metakey
+    # Parse common options from @options +Hash+ by metakey
     def use_options(metakey)
       if metakey == :margin || metakey == :padding
         value = [0, 0, 0, 0]
-        if @options[metakey].class == Array
+        if @options[metakey].is_a? Array
           value = (@options[metakey].map(&:to_i)*4)[0..3]
         else
           value = (@options[metakey].to_s.scan(/\-?\d+/).flatten.map(&:to_i)*4)[0..3] if @options[metakey] && @options[metakey].to_s =~ /\d/
         end
         [:top, :right, :bottom, :left].each_with_index {|side, index| value[index] = @options["#{metakey}_#{side}".to_sym].to_i if @options["#{metakey}_#{side}".to_sym] }
         instance_variable_set "@#{metakey}", value
+      elsif metakey == :border
+        border_sides_order = [:top, :right, :bottom, :left]
+        @options.keys.each do |key|
+          border_sides_order.push(border_sides_order.delete($1.to_sym)) if key.to_s =~ /\Aborder_(top|right|bottom|left)\Z/
+        end
+        @border = {}
+        [:top, :right, :bottom, :left].each do |side|
+          @options["#{metakey}_#{side}".to_sym] = @options[:border] unless @options["#{metakey}_#{side}".to_sym]
+          next unless @options["#{metakey}_#{side}".to_sym]
+          border = @options["#{metakey}_#{side}".to_sym].is_a?(Array) ? @options["#{metakey}_#{side}".to_sym] : @options["#{metakey}_#{side}".to_sym].to_s.split(/\s+/)
+          @border[side] = border[0..1] + [DynamicImageSources::SourceFactory.parse(border[2..-1])]
+          @border_sides_order = border_sides_order
+        end
       end
     end
 
@@ -124,7 +137,7 @@ module DynamicImageElements
     OPTIONS_ALIASES = {:w => :width, :h => :height, :bg => :background, :valign => :vertical_align}
 
     # Treats options Hash to generalize it and preparse sources
-    def treat_options(options)
+    def self.treat_options(options)
       #convert all keys to symbols
       options.keys.each do |key|
         next if key.class == Symbol
@@ -154,6 +167,9 @@ module DynamicImageElements
       options[:background] = DynamicImageSources::SourceFactory.parse options[:background] unless options[:background].to_s.empty?
       options[:color] = DynamicImageSources::SourceFactory.parse options[:color] unless options[:color].to_s.empty?
     end
+    def treat_options(options)
+      ElementInterface.treat_options options
+    end
 
     #drawing helpers
     def draw_background(x, y)
@@ -167,8 +183,72 @@ module DynamicImageElements
         context.restore
       end
     end
+
+    # Predefined styles for line drawing
+    LINE_STYLES = {
+      :solid => [],
+      :dotted => [1.0, 1.0],
+      :dashed => [3.0, 1.0],
+      :dashed_bigger => [5.0, 1.0],
+      :dashed_big => [8.0, 1.0],
+      :dashed_dot => [3.0, 1.0, 1.0, 1.0],
+      :dashed_double_dot => [3.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+      :dashed_big_bigger => [5.0, 1.0, 3.0, 1.0],
+      :double_dotted => [1.0, 1.0, 1.0, 3.0],
+      :triple_dotted => [1.0, 1.0, 1.0, 1.0, 1.0, 3.0],
+      :double_dashed_dot => [3.0, 1.0, 3.0, 1.0, 1.0, 1.0],
+      :double_dashed_double_dot => [3.0, 1.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+      :double_dashed_triple_dot => [3.0, 1.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0, 1.0],
+      :triple_dashed_dot => [3.0, 1.0, 3.0, 1.0, 3.0, 1.0, 1.0, 1.0],
+      :triple_dashed_double_dot => [3.0, 1.0, 3.0, 1.0, 3.0, 1.0, 1.0, 1.0, 1.0, 1.0]
+    }
+
     def draw_border(x, y)
-      #TODO
+      return unless @border_sides_order
+      original_source = context.source
+      lines_width = {
+        :top => (@border[:top][0].to_i rescue 0),
+        :right => (@border[:right][0].to_i rescue 0),
+        :bottom => (@border[:bottom][0].to_i rescue 0),
+        :left => (@border[:left][0].to_i rescue 0)
+      }
+      @border_sides_order.each do |key|
+        next unless @border && @border[key]
+        #line width
+        width = lines_width[key]
+        context.set_line_width width
+        #line style
+        multiple_gap = nil
+        if @border[key][1].to_s =~ /\A([a-z_]+)(\d+)\Z/
+          style = $1.to_sym
+          multiple_gap = $2.to_i
+        else
+          style = @border[key][1].to_sym rescue :solid
+        end
+        style = :solid unless LINE_STYLES[style]
+        style = LINE_STYLES[style].map{|i| i*width}
+        style = style.each_with_index.map{|i, index| index%2==1 ? i*multiple_gap : i} if multiple_gap
+        context.set_dash style, style.size
+        #line color
+        @border[key][2].set_source context
+        w, h = element_size
+        case key
+        when :top
+          context.move_to x-lines_width[:left], y-lines_width[:top]/2
+          context.line_to x+w+lines_width[:right], y-lines_width[:top]/2
+        when :right
+          context.move_to x+w+lines_width[:right]/2, y-lines_width[:top]
+          context.line_to x+w+lines_width[:right]/2, y+h+lines_width[:bottom]
+        when :bottom
+          context.move_to x+w+lines_width[:right], y+h+lines_width[:bottom]/2
+          context.line_to x-lines_width[:left], y+h+lines_width[:bottom]/2
+        when :left
+          context.move_to x-lines_width[:left]/2, y+h+lines_width[:bottom]
+          context.line_to x-lines_width[:left]/2, y-lines_width[:top]
+        end
+        context.stroke
+      end
+      context.set_source original_source
     end
   end
 end
