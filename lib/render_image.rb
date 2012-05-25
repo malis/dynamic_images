290f3d0b6055fcf5462b3083a4e1558314b0a3a9
Dominik Malis <dominik.malis@gmail.com>
2012-05-25 05:57:53 +0200

    added rdoc for rails support

--- lib/render_image.rb
+++ lib/render_image.rb
@@ -1,4 +1,15 @@
+# Module used to add methods to ActionController::Base
 module RenderImage
+  # Provides image drawing for Rails app controller. Default format is <tt>action_name.png</tt> in file called <tt>action_name.png.xml.erb</tt>.
+  #
+  # You can use different template name but it has to have extension <tt>.format.xml.erb</tt>. Passing extension <tt>.xml.erb</tt> is optional but file has be called with it.
+  #
+  # You can optionaly pass options (see below) and assigns too. Assigns is +Hash+ of keys and values which will be accessible from view as variables called as keys names.
+  #
+  # === Options
+  # [:quality]
+  #   When saving into JPEG format you can pass :quality into options. Valid values are in 0 - 100.
+  #
   def render_image(template = nil, options = {}, assigns = {})
     template ||= "#{action_name}.png"
     template += ".xml.erb" if template.class == String && template !~ /\.xml\.erb\Z/i
